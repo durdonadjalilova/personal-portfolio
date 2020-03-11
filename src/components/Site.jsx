122fd36385a12885406e1f761f2d306ee195c220
Suzette <suzetteislam@gmail.com>
2020-03-10 20:54:19 -0400

    populated main page after landing page with components to create an infinite scroll

--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import About from './About'
+import Contact from './Contact'
+import Footer from './Footer'
 
 const Site = () => (
-<div>
-    portfolio
-</div>
+    <div className='main'>
+        <div className='aboutMe'>
+            <About />
+        </div>
+
+        <div className='portfolio-projects'>
+
+        </div>
+        <div className ='skills-known'>
+
+        </div>
+
+        <div className='contact-form'>
+            <Contact/>
+        </div>
+
+        <div className ='site-footer'>
+            <Footer/>
+        </div>
+    </div>
 )
 
 export default Site
