c6421812f0950451eee4877fc4362919b1ef81e3
Suzette <suzetteislam@gmail.com>
2020-03-10 20:53:30 -0400

    modified projects js file and created display project component to map through projects and create card

---
+++ src/components/DisplayProjects.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+componentDidMount(){
+    createCard()
+}
+const createCard = () =>{
+    const {projects} = this.props
+    console.log(projects)
+
+}
+const DisplayProjects = () =>(
+    <div>
+
+    </div>
+)
+
+export default DisplayProjects
---
+++ src/components/Projects.js
@@ -0,0 +1,23 @@
+const projects = [
+    {
+        title: 'Simply 21',
+        description: 'A web Black Jack game built using the Deck Of Cards API that allows users to play against the computer. The App allows users to draw cards, hit or stay, draws cards for the computer, whoever’s score is closest to 21 wins.',
+        github: '',
+        live: '#'
+    },
+    {
+        title: 'Wanderfull',
+        description: 'A social media web application where users can track locations they have traveled to with photo posts of landscape and food.',
+        github: '',
+        live: '#'
+    },
+    {
+        title: 'MemeSpace',
+        description: 'A search engine for memes. Users will be able to make posts with their meme of choice and have it saved to a local database.',
+        github:'',
+        live:'#'
+    }
+
+]
+
+export default projects
