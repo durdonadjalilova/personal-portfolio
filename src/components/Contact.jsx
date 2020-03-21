553e1815326e7620fe6a8e553044c0e762bc8557
Suzette <suzetteislam@gmail.com>
2020-03-21 17:52:23 -0400

    contact form works

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 
 const Contact = () => (
-<div className='contactSection'>
-    <div className='contactText'>
-        <p>Please leave a message after the click!</p>
+    <div className='contactSection'>
+        <div className='contactText'>
+            <p>Contact me! Please fill out the form with your message or reach me at suzetteislam@gmail.com and I'll get back as soon as possible.</p>
+        </div>
+        <form className='contact' action="https://formspree.io/xoqlkzqz" method="POST">
+            <label for='name'>Name:</label>
+            <input type='text' placeholder='name' name='name' />
+            <br></br>
+            <label for='email'>Email:</label>
+            <input type="email" placeholder='email' />
+            <br></br>
+            <label for='message'>Message:</label>
+            <input type="text" placeholder='message' />
+            <br></br>
+            <button>Send!</button>
+        </form>
     </div>
-    <form className='contact'>
-
-        <input type="text" placeholder='email'/>
-        <br></br>
-        <input type="text" placeholder='message'/>
-        <br></br>
-       <a href ='mailto:suzetteislam@gmail.com'><button>Send!</button></a>
-    </form>
-</div>
 )
 
 export default Contact
