d3d8959a1f37205611a0e8e41f7d6e164404e6d4
Suzette <suzetteislam@gmail.com>
2020-03-21 10:08:44 -0400

    created temp navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link, Route } from 'react-router-dom'
+
+const NavBar = () => (
+    <div>
+        <nav>
+            <label htmlFor="hamburger">&#9776;</label>
+            <input type="checkbox" id='hamburger' />
+            
+            <ul>
+                <li><Link>About</Link></li>
+                <li><Link>Projects</Link></li>
+                <li><Link>Skills</Link></li>
+                <li><Link>Contact</Link></li>
+            </ul>
+
+        </nav>
+    </div>
+)
+
+export default NavBar
