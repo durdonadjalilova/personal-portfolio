535590178c18e24ced346832fcf9d166994cf147
Suzette <suzetteislam@gmail.com>
2020-06-11 01:32:51 -0400

    modified form styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 const Contact = () => (
     <div className='contactSection' id='contact'>
         <div className='contactText'>
-            <p>Contact me! <br></br>Please fill out the form with your message or reach me at suzetteislam@gmail.com.</p>
+            <h3>Contact me!</h3>
+            <p>Please fill out the form with your message or reach me at suzetteislam@gmail.com.</p>
         </div>
         <div className='formholder'>
         <form className='contact' action="https://formspree.io/xoqlkzqz" method="POST">
-            <label for='name'>Name:</label>
-            <input type='text' placeholder='name' name='name' />
-            <label for='email'>Email:</label>
-            <input type="email" placeholder='email' />
-            <br></br>
+            <label for='name'>Name:</label><br/>
+            <input type='text' placeholder='name' name='name' /><br/>
+            <label for='email'>Email:</label><br/>
+            <input type="email" placeholder='email' /><br/>
             <br/>
-            <label for='message'>Message:</label>
+            <label for='message'>Message:</label><br/>
             <input type="text" placeholder='message' />
             <br></br>
             <button>Send!</button>
