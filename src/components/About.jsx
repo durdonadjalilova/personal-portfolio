2f5e383cb1190b1c44ddfd48469b0e85f4b19317
Suzette <suzetteislam@gmail.com>
2020-06-11 01:33:33 -0400

    added icon links for github, linkedin, resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,34 +1,41 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
 import Resume from '../additional/resume.pdf'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import Selfie from '../additional/selfie.jpeg'
 import Skyline from '../additional/nyc.png'
 
-const About = () => (
-    
+const About = () => {
+
+    const linkedIn = () => {
+        const url = 'https://www.linkedin.com/in/suzetteislam'
+        window.open(url, '_blank')
+    }
+
+    const github = () => {
+        const url = 'https://www.github.com/sislam16'
+        window.open(url, '_blank')
+    }
+
+    // const resume =() =>{
+    //     const url = {Resume}
+    //     window.open(url, '_blank')
+    // }
+
+    return (
         <div className='aboutSection' id='about'>
-            
-            <div className='aboutTxt'>
+            <img src={Selfie} alt='suzette'className='aboutImg' />
+
+            <div className='abtTxt'>
                 <p>I'm a full stack web developer based in New York City. </p>
-                {/* <p>con</p>
-                <p>lksd</p> */}
-
                 <div className='links'>
-                    <p>hi</p>
+                    <img src="https://img.icons8.com/color/48/000000/linkedin.png" alt='icon' onClick={linkedIn} />
+                    <img src="https://img.icons8.com/ios-glyphs/50/000000/github.png" alt='icon' onClick={github}/>
+                    <a href={Resume} target='_blank'><img src="https://img.icons8.com/windows/40/000000/file.png" alt ='file'/></a>
 
-                    {/* <i class="fab fa-github" hred='https://www/github.com/sislam16' target='_blank'></i> */}
-
-
-                    {/* <a href="https://www.github.com/sislam16" target="_blank" className='aboutIcon'><FontAwesomeIcon className='icon' icon={['fab', 'github']} /></a>
-                    <a href="https://www.linkedin.com/in/suzetteislam" target="_blank" className='aboutIcon'><i className="fab fa-linkedin"></i></a>
-                    <a href={Resume} target="_blank" className='aboutIcon'><i className="fas fa-file-alt"></i></a> */}
-           </div> 
-
+                </div>
             </div>
-            <a href='#projects'>Projects</a>
-            <img src = {Skyline} id='skyline'/>
+            <img src={Skyline} id='skyline' alt='landing' />
         </div>
-)
+    )
+}
 
 export default About
