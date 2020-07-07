057bfc046c694371107e143756907998b309ac1e
Suzette <suzetteislam@gmail.com>
2020-07-06 23:26:01 -0400

    moved github and linkedin icon to aboutme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Skills from './components/Skills';
-// import Slogo from './additional/si.png';
 import projects from './components/Projects'
 import Resume from './additional/resume.pdf'
 import Skyline from './additional/nyc.png'
@@ -11,14 +10,14 @@ import Logo from './additional/white-logo.png'
 const cards = projects.map(el => {
   return (
     <div className='project-card'>
-      <img src = {el.img} alt='project'/>
+      <img src={el.img} alt='project' />
       <div className='back-card'>
-      <h2>{el.title}</h2>
-      <p>{el.description}</p>
-      <a href={el.github}>GitHub</a>
-      <a href={el.live}>Live</a>
+        <h2>{el.title}</h2>
+        <p>{el.description}</p>
+        <a href={el.github}>GitHub</a>
+        <a href={el.live}>Live</a>
       </div>
-      
+
     </div>
 
   )
@@ -38,7 +37,7 @@ function App() {
   return (
     <div className="App">
       <div className='nav'>
-        <div className='logo'><img src={Logo} id='si' alt='logo' style={{ marginLeft: '10px', height: '50px', marginTop:'5%' }} /></div>
+        <div className='logo'><img src={Logo} id='si' alt='logo' style={{ marginLeft: '10px', height: '50px', marginTop: '5%' }} /></div>
         <ul>
           <li><a href='#About'>about</a></li>
           <li><a href='#Projects'>projects</a></li>
@@ -48,12 +47,7 @@ function App() {
       </div>
       <div className='landing' id='landing'>
         <h1 id='greeting' style={{ fontWeight: 'bold' }}>Suzette Islam</h1>
-        <div className='links'>
-          <img src="https://img.icons8.com/color/48/000000/linkedin.png" alt='icon' onClick={linkedIn} />
-          <img src="https://img.icons8.com/ios-glyphs/50/000000/github.png" alt='icon' onClick={github} />
-          {/* <a href={Resume} rel ='noopener noreferrer' target='_blank'><img src="https://img.icons8.com/windows/40/000000/file.png" alt='file' /></a> */}
 
-        </div>
         <img src={Skyline} id='skyline' alt='landing' />
 
       </div>
@@ -68,13 +62,18 @@ function App() {
 
           <div id='txt'>
             <p>Hey everyone! I'm Suzette, a full stack web developer based in New York City. My journey in tech began when I was exposed to HTML and CSS in middle school. This interest was one that I kept in the back of my mind until I reached college. While I was unable to obtain a Computer Science degree I did extensive research to ensure I accomplished my goal afterwards. I later found Pursuit and was admitted as a Full Stack Web Development Fellow. I am excited to continue learning about the industry and look forward to starting a new chapter as a web developer. Please check out my project below and feel free to contact me via email at <span style={{ fontStyle: 'italic', fontWeight: 'bold' }}>suzetteislam@gmail.com</span> or by filling out the contact form below! </p>
+              <div className='links' >
+                <img src="https://img.icons8.com/color/48/000000/linkedin.png" alt='icon' onClick={linkedIn} />
+                <img src="https://img.icons8.com/ios-glyphs/50/000000/github.png" alt='icon' onClick={github} />
+
+              </div>
           </div>
 
         </div>
 
         <div className='skills'>
           <div><h3 id='technologies' style={{ textDecoration: 'underline', textDecorationColor: '#EB9605', fontSize: '22px' }}>Technologies I've worked with:</h3></div>
-          <br/>
+          <br />
           <Skills />
         </div>
       </div>
@@ -100,9 +99,9 @@ function App() {
         </div>
       </div>
 
-      <div className='footer' style={{display:'flex', justifyContent:'space-between'}}>
-        <p style={{marginLeft:'2%'}}>© Suzette Islam</p>
-        <p style={{marginRight:'2%'}}>Thank you for visiting!</p>
+      <div className='footer' style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <p style={{ marginLeft: '2%' }}>© Suzette Islam</p>
+        <p style={{ marginRight: '2%' }}>Thank you for visiting!</p>
       </div>
 
     </div>
